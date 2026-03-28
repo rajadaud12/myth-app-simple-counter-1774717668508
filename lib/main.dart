324e7c54f9885ed91c0d922449a8c1373ea4a8e3
Daud Bin Nasar <64163448+rajadaud12@users.noreply.github.com>
2026-03-28 22:08:00 +0500

    Update lib/main.dart via MYTH Build System

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:simple_counter_app/screens/home.dart'; 
+import 'package:simple_counter_app/utils/colors.dart'; 
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(builder: (context, child) => MediaQuery(data: MediaQuery.of(context).copyWith(padding: const EdgeInsets.only(top: 44.0, bottom: 34.0)), child: child!), 
+      title: 'Simple Counter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.primary,
+          brightness: Brightness.light,
+        ),
+        scaffoldBackgroundColor: AppColors.background,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
